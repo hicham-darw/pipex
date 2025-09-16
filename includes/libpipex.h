a4942be4a52cae8181289b4a36d8639bf13dbec2
hicham-darw <hamoudhicham234@gmail.com>
2025-09-16 09:12:52 +0100

    fix norms

--- includes/libpipex.h
+++ includes/libpipex.h
@@ -11,9 +11,11 @@
 void	write_to_pipe(int fds[], char **av, char **env);
 void	write_to_file(int fds[], char **av, char **env);
 char	*get_pathname_execve(char *cmd, char **env);
-int		fork_failed();
-void	perror_and_exit(void (*perr)(const char *), const char *s);
+int		fork_fails(void);
 void	fill_args_execve(char **cmd, char *args[]);
-void	dup_and_close(int old_fd, int new_fd);
+void	dup2_and_close(int old_fd, int new_fd);
+void	execve_fails(char *pathname, char **args);
+int		put_usage(void);
+void	put_command_not_found_fd(char *err_msg, char *cmd);
 
 #endif
