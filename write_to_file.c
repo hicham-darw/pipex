fe3cfeba6d07c91c60556d4a9225ffeacbcd3fb0
hicham-darw <hamoudhicham234@gmail.com>
2025-09-16 09:13:24 +0100

    memory no leak :)

--- write_to_file.c
+++ write_to_file.c
@@ -1,34 +1,30 @@
 #include "libpipex.h"
 
-void    write_to_file(int fds[], char **av, char **env)
+void	write_to_file(int fds[], char **av, char **env)
 {
-    char    **command;
-    char    *pathname;
-    int fd;
+	char	**command;
+	char	*pathname;
+	int		fd;
 
-    close(fds[1]);
-    fd = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (fd == -1)
-    {
-        perror("open failed");
-        exit(1);
-    }
-    dup2(fd, STDOUT_FILENO);
-    close(fd);
-    command = ft_split(av[3], ' ');
-    if (!command)
-        exit(42);
-    pathname = get_pathname_execve(command[0], env);
-    if (!pathname)
-    {
-        fprintf(stderr, "%s: command not found\n", command[0]);
-        exit(127);
-    }
-    dup2(fds[0], STDIN_FILENO);
-    close(fds[0]);
-    execve(pathname, command, env);
-    ft_delvector(command);
-    free(pathname);
-    perror("execve");
-    exit(1);
+	close(fds[1]);
+	fd = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd == -1)
+	{
+		perror(av[4]);
+		exit(1);
+	}
+	dup2_and_close(fd, STDOUT_FILENO);
+	command = ft_split(av[3], ' ');
+	if (!command)
+		exit(1);
+	pathname = get_pathname_execve(command[0], env);
+	if (!pathname)
+	{
+		put_command_not_found("command not found", command[0]);
+		ft_delvector(command);
+		exit(127);
+	}
+	dup2_and_close(fds[0], STDIN_FILENO);
+	execve(pathname, command, env);
+	execve_fails(pathname, command);
 }
