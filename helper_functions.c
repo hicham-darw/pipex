47531a8e0d013e50ba3fa108840984ae2918dff3
hicham-darw <hamoudhicham234@gmail.com>
2025-09-16 09:12:28 +0100

    rm & add helper functions

--- helper_functions.c
+++ helper_functions.c
@@ -1,19 +1,34 @@
 #include "libpipex.h"
 
-int	fork_failed()
+int	fork_fails(void)
 {
-	printf("fork failed");
+	perror("fork failed");
 	return (1);
 }
 
-void	perror_and_exit(void (*perr)(const char *), const char *s)
-{
-	perr(s);
-	exit(EXIT_FAILURE);
-}
-
-void	dup_and_close(int fd_file, int fd)
+void	dup2_and_close(int fd_file, int fd)
 {
 	dup2(fd_file, fd);
 	close(fd_file);
+}
+
+void	execve_fails(char *pathname, char **args)
+{
+	free(pathname);
+	ft_delvector(args);
+	perror("execve");
+	exit(1);
+}
+
+void	put_command_not_found_fd(char *err_msg, char *cmd)
+{
+	ft_putstr_fd(cmd, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putendl_fd(err_msg, 2);
+}
+
+int	put_usage(void)
+{
+	ft_putendl_fd("Usage: ./pipex file1 cmd1 cmd2 file2", 2);
+	return (1);
 }
